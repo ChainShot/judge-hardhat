6c79a907b3b41d65d56a26dbcc06fc860e738273
Dan <DanNolan99@gmail.com>
2022-07-04 13:34:39 -0400

    init

---
+++ content/contracts/Simple6.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.6.0;
+
+contract Simple6 {
+  uint public a = 5;
+}
---
+++ content/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomiclabs/hardhat-waffle");
+
+const settings = {
+  optimizer: {
+    enabled: false,
+    runs: 0
+  }
+}
+
+const versions = ["0.6.12", "0.7.5", "0.8.4"];
+
+module.exports = {
+  solidity: {
+    compilers: versions.map((version) => ({ version, settings }))
+  }
+};
